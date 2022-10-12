aebe0bceb006a456c034cd830bd963bcbaea73fc
Zavaruev Vladislav <tel.8.999.995.99.93@gmail.com>
2022-10-12 22:08:25 +0300

    add error correction

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,29 +4,18 @@ function solveEquation(a, b, c) {
   let arr = [];
 
   // код для задачи №1 писать здесь
-  if(a == 0)
-    return "Ошибка: A = 0";
-  
-  let d = b * b - 4 * a * c; // 1
-  console.log((d));
+   let d = b * b - 4 * a * c;
+  console.log(d);
 
-  if(d < 0) // 2
-    return "Корней нет"
-  if(d == 0) // 3
-    arr["quadratic roots"] = -b / (2 * a);
-  else if (d > 0) {
-    let tmp = [] // 4
-      arr.push((-b + Math.sqrt(d)) / (2 * a));
-      arr.push((-b - Math.sqrt(d)) / (2 * a));
-    arr["quadratic roots"] = tmp;
+  if(a == 0) {
+    return "Ошибка: A = 0";
+  } if(d == 0) {
+    arr.push((-b) / (2 * a));
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
   }
   //
 
-  return arr; // array
+  return arr;
 }
-
-// Результат []
-// 1. D = b^2-4*a*c | Math.pow() +
-// 2. D < 0 = [корней нет (пустой массив)]
-// 3. D = 0 [1 корень]| Вычислить и вернуть из функции | -b/(2*a)
-// 4. D > 0 [2 кореня]| Вычислить и вернуть из функции | (-b + Math.sqrt(d) )/(2*a) и (-b - Math.sqrt(d) )/(2*a)
