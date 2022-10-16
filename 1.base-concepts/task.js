bc4ecf7491d2ab9e0279b3fef6c8df76e715ff78
Zavaruev Vladislav <tel.8.999.995.99.93@gmail.com>
2022-10-16 20:01:05 +0300

    add task one

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -9,7 +9,9 @@ function solveEquation(a, b, c) {
 
   if(a == 0) {
     return "Ошибка: A = 0";
-  } if(d == 0) {
+  } 
+  
+  if(d == 0) {
     arr.push((-b) / (2 * a));
   } else if (d > 0) {
     arr.push((-b + Math.sqrt(d)) / (2 * a));
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,23 +1,45 @@
+"use strict"
+
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = 0;
+  let sum = 0;
 
-  // Ваш код
+  // Cумму всех элементов
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  // Максимальное и Минимальное значение
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] > max) {
+      max = arr[i];
+    } 
+    if(arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  // Среднее арифметическое значение
+  let count = ((sum / arr.length).toFixed(2));
+  let avg = Number(count);
 
   return { min: min, max: max, avg: avg };
 }
 
+getArrayParams([5])
+
 // Задание 2
-function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
+const f = sum => {
+  return sum.map(nums => nums.reduce((acc, v) => acc + v, 0));
 }
+  
+const sum = [[5, 5, 11], [10, 10, 17], [10, 5]];
+console.log(f(sum));
 
-function makeWork(arrOfArr, func) {
-  let max;
+function makeWork(arrOfArr, func) { // Мясорубка
+  let max = 0;
 
   // Ваш кода
   // for ...
