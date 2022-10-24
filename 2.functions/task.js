270fcd61e63583a9623c441312785d4372139480
Zavaruev Vladislav <tel.8.999.995.99.93@gmail.com>
2022-10-24 18:25:47 +0300

    add task three

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,8 +2,8 @@
 
 // Задание 1
 function getArrayParams(arr) {
-  let min = arr[0];
-  let max = 0;
+  let min = Infinity;
+  let max = -Infinity;
   let sum = 0;
 
   for (let i = 0; i < arr.length; i++) {
@@ -28,6 +28,7 @@ function getArrayParams(arr) {
 
 getArrayParams([-99, 99, 10])
 
+
 // Задание 2
 function worker(arr) {
   let sum = 0;
@@ -39,11 +40,11 @@ function worker(arr) {
   return sum;
 }
 
-function makeWork(arrOfArr, worker, worker2) {
+function makeWork(arrOfArr, func) {
   let max = 0;
 
   for (let i = 0; i < arrOfArr.length; i++) {
-    let sum = worker(arrOfArr[i]);
+    let sum = func(arrOfArr[i]);
 
     if (max < sum) { 
       max = sum;
@@ -53,12 +54,28 @@ function makeWork(arrOfArr, worker, worker2) {
   return max;
 }
 
-// console.log(makeWork([[10,10,20],[20,19,10]],worker));
+// Вывод 2 Задания
+console.log(worker([1, 2, 3])); // 6
+console.log(worker([4, 5, 6])); // 15
+let arrOfArr = [[1, 2, 3], [4, 5, 6]];
+makeWork(arrOfArr, worker); // 15
+
+console.log(worker([10, 10, 11])); // 31
+console.log(worker([20, 10])); // 30
+arrOfArr = [[10, 10, 11], [20, 10]];
+makeWork(arrOfArr, worker); // 31
+
+console.log(worker([0, 0, 0])); // 0
+console.log(worker([-1, -100])); // -101
+arrOfArr = [[0, 0, 0], [-1, -100]];
+makeWork(arrOfArr, worker); // 0
+//
+
 
 // Задание 3
 function worker2(arr) {
-  let min = arr[0];
-  let max = 0;
+  let min = Infinity;
+  let max = -Infinity;
 
   for (let i = 0; i < arr.length; i++) {
     if(arr[i] > max) {
@@ -67,11 +84,19 @@ function worker2(arr) {
     if(arr[i] < min) {
       min = arr[i];
     }
-
-    let difference = Math.abs(max - min);
-    
-    return difference;
   }
+    
+  return Math.abs(max - min);
 }
 
-// console.log(worker2([-10, -20, -40])); // -40 - (-10) = -30 => 30
+// Вывод 3 Задания
+console.log(worker2([-10, -20, -40])); // -40 - (-10) = -30 => 30
+console.log(worker2([10, 20, 30])); // 30 - 10 = 20
+arrOfArr = [[-10, -20, -40], [10, 20, 30]];
+makeWork(arrOfArr, worker2); // 30
+
+console.log(worker2([0, 0, 0])); // 0 - 0 = 0
+console.log(worker2([-1, -99])); // -99 - (-1) = -98 => 98
+arrOfArr = [[0, 0, 0], [-1, -99]];
+makeWork(arrOfArr, worker2); // 98
+//
