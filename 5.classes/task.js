edfac833c8175ad4215e69bb559593a6b5535110
Zavaruev Vladislav <tel.8.999.995.99.93@gmail.com>
2022-12-03 21:15:03 +0300

    add task with classes - task two

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,3 +1,5 @@
+"use strict"
+
 class PrintEditionItem {
     constructor (name, releaseDate, pagesCount) {
         this.name = name;
@@ -8,57 +10,94 @@ class PrintEditionItem {
     }
 
     fix() {
-        return this.state += 1.5;              // тут скорее всего зависит от state типа от 50% начинает прибавлять по 1.5 ?
-    }
-
-    set state(value) {                         // если это скрыть, то работает > создание печатного издания, что-то тут не так ?
-        if (value < 0) {
-            this.state = 0;
-        } else if (value > 100) {
-            this.state = 100;
-        } else {
-            return value;
-        }
+        return this.state *= 1.5;
     }
 
     get state() {
-        return this.value;
+        return this._state;
+    }
+
+    set state(value) {
+        this._state = value;
+
+        if (value < 0) {
+            value = 0;
+        } else if (value > 100) {
+            value = 100;
+        } else {
+            value;
+        }
     }
 }
 
-class Magazine extends PrintEditionItem {      // срабатывает 1 вариант наследования
-    constructor (name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
+// Наследники:
+
+class Magazine extends PrintEditionItem {      /*Наследник 1*/
+    constructor (...args) {
+        super(...args);
         this.type = `magazine`;
     }
 }
 
-
-class Book extends PrintEditionItem {          // далее не работает
-    constructor () {
-        super();
+class Book extends PrintEditionItem {          /*Наследник 2*/ // + this.type = `book`;   + this.author = `author`;
+    constructor (author, ...args) {
+        super(...args);
+        this.author = author;
         this.type = `book`;
-        this.author = `author`;
     }
 }
 
-class NovelBook extends Book {
-    constructor () {
-        super();
+class NovelBook extends Book {                 /*Наследник 3*/ // + this.type = `novel`;
+    constructor (...args) {
+        super(...args);
         this.type = `novel`;
     }
 }
 
-class FantasticBook extends Book {
-    constructor () {
-        super();
-        this.type = `fantastic`;
+class FantasticBook extends Book {             /*Наследник 4*/ // + this.type = `fantastic`;
+    constructor (...args) {
+        super(...args);
+        this.type = `fantastic`
     }
 }
 
-class DetectiveBook extends Book {
-    constructor () {
-        super();
+class DetectiveBook extends Book {             /*Наследник 5*/ // + this.type = `detective`;
+    constructor (...args) {
+        super(...args);
         this.type = `detective`;
+    }
+}
+
+/* Задача №2. Библиотека */
+
+// 1 Добавление книги
+
+class Library {
+    constructor (name) {
+        this.name = name;
+        this.books = [];
+    }
+
+
+    // 1 Добавить книгу.
+    // Добавляет книгу, если state > 30
+    addBook(book) {
+        this.books.push(book);
+    }
+
+
+    // 2 Поиск книги.
+    // который в качестве аргумента будет принимать ключ, по которому идёт поиск (тип, автор, название, год выпуска и пр.) и искомое значение. 
+    // Метод должен возвращать книгу в случае успеха и null, если книга не найдена.
+    findBookBy(type, value) {
+
+    }
+
+
+    // 3 Выдача книги.
+    // Который будет в качестве аргумента принимать название книги. Если запрошенная книга найдена, 
+    // Метод должен удалять книгу из хранилища books и возвращать её. Если книга не была найдена, метод должен возвращать null.
+    giveBookByName(bookName) {
+
     }
 }
